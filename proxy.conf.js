3e8360a00cbd5f96ee48d23673ce4df6d1996eb2
Andrea Sarmiento <anbesamo@gmail.com>
2022-05-13 20:44:20 -0400

    save proxy

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,10 +1,11 @@
 
-const PROXY_CONFIG ={
+   const PROXY_CONFIG ={
     '/api':{
-        'target': "http://upb-alb-1098414869.us-east-1.elb.amazonaws.com:3306",
+        'target': "http://upb-alb-233930559.us-east-1.elb.amazonaws.com:3306",
          'pathRewrite':{
              '^/api':''
-         }
+         },
+         'secure':false
     }
 }
 module.exports =  PROXY_CONFIG
